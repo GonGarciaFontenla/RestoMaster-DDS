d22582094d073d8b8d8d71d27a1536ae32000713
GonGarciaFontenla <ggarciafontenla@frba.utn.edu.ar>
2026-04-17 09:25:56 -0300

    entrega3

--- src/domain/Mesa.js
+++ src/domain/Mesa.js
@@ -1,14 +1,6 @@
 import { EstadoMesa } from "./enums/EstadoMesa.js";
 import { Ubicacion } from "./enums/Ubicacion.js";
 
-/**
- * Representa una mesa física del restaurante.
- *
- * @param {number} numero - Número identificador de la mesa.
- * @param {number} capacidad - Cantidad máxima de comensales.
- * @param {Ubicacion} ubicacion - Sector del restaurante donde se encuentra.
- * @param {EstadoMesa} estado - Estado actual de la mesa (por defecto: LIBRE).
- */
 export default class Mesa {
   constructor(numero, capacidad, ubicacion, estado = EstadoMesa.LIBRE) {
     this.numero = numero;
